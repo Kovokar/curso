bdda780e113e8ffa3f6b14e856b56431fb9fdc3c
kovokar <pkovokar@gmail.com>
2026-01-09 00:11:32 -0300

    lesson 37 static method

--- OOP_py/pessoa.py
+++ OOP_py/pessoa.py
@@ -1,6 +1,6 @@
 #aula 36
 from typing import Self, ClassVar
-
+from random import randint
 class Pessoa:
     ano_atual: ClassVar[int] = 2026
 
@@ -15,7 +15,11 @@ class Pessoa:
     def por_ano_nascimento(cls, nome: str, ano_nascimento: int) -> Self:
         idade: int = cls.ano_atual - ano_nascimento
         return cls(nome, idade)
+    
+    @staticmethod
+    def gerar_id():
+        return randint(0,10**6)
 
 
 p1 = Pessoa.por_ano_nascimento("Felipe", 2006)
-print(p1.idade)
+print(p1.gerar_id())
