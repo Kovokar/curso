da3721532b6bccb26cd4cfc5d198213875f0d0e1
kovokar <pkovokar@gmail.com>
2026-01-09 00:29:55 -0300

    lesson 38 getter setter

---
+++ OOP_py/produto.py
@@ -0,0 +1,26 @@
+#lesson 38 getter setter 
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+        
+    def desconto(self, percentual):
+        self.preco = self.preco - (self.preco * (percentual/100))
+
+    # getter
+    @property
+    def preco(self):
+        return self._preco
+    
+    #setter
+    @preco.setter
+    def preco(self, valor):
+        if isinstance(valor, str):
+            valor = float(valor.replace('R$', ''))
+        self._preco = valor
+        
+        
+p1 = Produto("jooa", 'R$ 50')
+p1.desconto(50)
+print(p1.preco)
